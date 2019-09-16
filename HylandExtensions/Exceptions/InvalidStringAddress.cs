a119a908fc55588b921dbb3c2517e97b6485b094
Justin Baur <136baur@gmail.com>
2019-09-16 09:23:56 -0400

    Add files via upload

---
+++ HylandExtensions/Exceptions/InvalidStringAddress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HylandAddons.Exceptions
+{
+    public class InvalidStringAddressException : Exception
+    {
+        public string Address { get; }
+
+        public InvalidStringAddressException(string address) : base($"Invalid string address: { address }")
+        {
+            Address = address;
+        }
+    }
+}
