be8981bbf5c29f32c6ea7c4ed43a58816ac9ee26
Justin Baur <136baur@gmail.com>
2019-09-16 10:22:09 -0400

    Change to path to keep naming scheme

---
+++ HylandAddons/Attributes/OptOutAttributeSerializationAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HylandAddons.Attributes
+{
+    [System.AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class OptOutAttributeSerializationAttribute : System.Attribute { }
+}
---
+++ HylandAddons/Attributes/WorkViewAttributeAddressAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HylandAddons.Attributes
+{
+    [System.AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class WorkViewAttributeAddressAttribute : System.Attribute
+    {
+        public string StringAddress { get; }
+
+        public WorkViewAttributeAddressAttribute(string stringAddress)
+        {
+            StringAddress = stringAddress;
+        }
+    }
+}
